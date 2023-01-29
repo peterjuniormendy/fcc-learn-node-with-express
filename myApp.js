ad6ffae08e395de51c823d602faa00ab20bec130
Peter Junior Mendy <petermendy5@gmail.com>
2023-01-29 18:53:40 +0000

    serve a json response)

--- myApp.js
+++ myApp.js
@@ -8,10 +8,13 @@ app.use("/public", express.static(__dirname + "/public"));
 console.log("Hello World");
 
 // EXPRESS ROUTE THAT SENDS A FILE AS A RESPONCE
-
 app.get("/", function (req, res) {
   //   res.send("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
