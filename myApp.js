5bf16199ca6913593e4a7e088568136810d37e38
Peter Junior Mendy <petermendy5@gmail.com>
2023-01-29 18:03:02 +0000

    serve an HTML file)

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  //   res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
